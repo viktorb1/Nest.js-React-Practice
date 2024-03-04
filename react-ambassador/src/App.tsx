4f08a170dcaa7aa4dea7b3143216a7c896fc308e
Viktor Babchanik <vbabchanik@ucdavis.edu>
2024-03-04 14:10:07 -0800

    Fix folder placement

--- react-ambassador/src/App.tsx
+++ react-ambassador/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ProductsFrontend from './pages/ProductsFrontend';
 
 
 function App() {

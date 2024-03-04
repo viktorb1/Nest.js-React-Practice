a5c4d9c4e7616da240056b70203cd314b3d31d64
Viktor Babchanik <vbabchanik@ucdavis.edu>
2024-03-04 14:16:30 -0800

    This commit works, ignore all the others

--- react-ambassador/src/App.tsx
+++ react-ambassador/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ProductsFrontend from './pages/ProductsFrontend';
 

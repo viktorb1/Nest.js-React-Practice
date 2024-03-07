7f95b55f1600040093813652dbae9591ed512d15
Viktor Babchanik <vbabchanik@ucdavis.edu>
2024-03-07 01:02:01 -0800

    Finish react ambassador app

--- react-ambassador/src/pages/ProductsBackend.tsx
+++ react-ambassador/src/pages/ProductsBackend.tsx
@@ -3,22 +3,44 @@ import Layout from '../components/Layout'
 import Products from './Products'
 import { Product } from '../models/product'
 import axios from 'axios'
+import { Filters } from '../models/filters'
 
 const ProductsBackend = () => {
   const [products, setProducts] = useState<Product[]>([])
+  const [filters, setFilters] = useState<Filters>({
+    s: '',
+    sort: '',
+    page: 1
+  })
+  const [lastPage, setLastPage] = useState(0)
 
   useEffect(() => {
     (
       async () => {
-        const {data} = await axios.get('products/backend')
-        setProducts(data.data)
+        const arr = [];
+
+        if (filters.s) {
+          arr.push(`s=${filters.s}`)
+        }
+
+        if (filters.sort) {
+          arr.push(`sort=${filters.sort}`)
+        }
+
+        if (filters.page) {
+          arr.push(`page=${filters.page}`)
+        }
+
+        const {data} = await axios.get(`products/backend?${arr.join('&')}`)
+        setProducts(filters.page === 1 ? data.data : [...products, ...data.data])
+        setLastPage(data.last_page)
       }
     )();
-  }, [])
+  }, [filters])
   
   return (
     <Layout>
-      <Products products={products} />
+      <Products products={products} filters={filters} setFilters={setFilters} lastPage={lastPage}/>
     </Layout>
   )
 }
--- react-ambassador/src/pages/ProductsFrontend.tsx
+++ react-ambassador/src/pages/ProductsFrontend.tsx
@@ -3,23 +3,71 @@ import Layout from '../components/Layout'
 import Products from './Products'
 import axios from "axios"
 import { Product } from '../models/product'
+import { Filters } from '../models/filters'
 
 
 const ProductsFrontend = () => {
-  const [products, setProducts] = useState<Product[]>([])
+  const [allProducts, setAllProducts] = useState<Product[]>([])
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([])
+  const [filters, setFilters] = useState<Filters>({
+    s: '',
+    sort: '',
+    page: 1
+  })
+  const perPage = 9;
+  const [lastPage, setLastPage] = useState(0)
+
 
   useEffect(() => {
     (
       async () => {
         const {data} = await axios.get('products/frontend')
-        setProducts(data)
+        setAllProducts(data)
+        setFilteredProducts(data)
+        setLastPage(Math.ceil(data.length / perPage))
       }
     )();
   }, [])
 
+  useEffect(() => {
+    let products = allProducts.filter(p => p.title.toLowerCase().indexOf(filters.s.toLowerCase()) >= 0 ||
+    p.description.toLowerCase().indexOf(filters.s.toLowerCase()) >= 0)
+
+    if (filters.sort === 'asc') {
+      products.sort((a, b) => { 
+        if (a.price > b.price) {
+          return 1
+        }
+
+        if (a.price < b.price) {
+          return -1;
+        }
+
+        return 0
+      })
+    }
+
+    if (filters.sort === 'desc') {
+      products.sort((a, b) => { 
+        if (a.price > b.price) {
+          return -1
+        }
+
+        if (a.price < b.price) {
+          return 1;
+        }
+
+        return 0
+      })
+    }
+
+    setFilteredProducts(products.slice(0, filters.page * perPage))
+    setLastPage(Math.ceil(products.length / perPage))
+  }, [filters])
+
   return (
     <Layout>
-      <Products products={products} />
+      <Products products={filteredProducts} filters={filters} setFilters={setFilters} lastPage={lastPage}/>
     </Layout>
   )
 }
